4554ddc91c80c023059c453d90ae4598d2f96ad7
rob <robertcasson11@gmail.com>
2023-12-15 07:25:40 +0000

    CHORE: CSS updated to add borders to sections, general colour scheme created and positioning of nav and main elements

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,14 @@ import React from "react";
 const About = () => {
   return (
     <section className="about">
-      <p>about</p>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam cursus
+        tincidunt orci eu interdum. Proin scelerisque euismod ligula. Nullam
+        tempus velit et nisl ultrices egestas. Morbi pellentesque interdum quam.
+        Integer id velit eu odio pulvinar consequat id at risus. Donec quis diam
+        magna. Aenean gravida neque ut iaculis rhoncus. Pellentesque.
+      </p>
     </section>
   );
 };
